f2a7f304f5c821e99dae7a152b1bddd4c6deb76f
Slinthn <im@slinthn.me>
2023-01-15 18:01:23 +0000

    More functions put into files

--- src/graphics/vulkan_init.c
+++ src/graphics/vulkan_init.c
@@ -7,6 +7,9 @@
 
 #include "vulkan.h"
 #include "vulkan_win64.c"
+#include "vulkan_render.c"
+#include "vulkan_shader.c"
+#include "vulkan_buffer.c"
 
 #ifdef SLN_DEBUG
 
@@ -407,42 +410,6 @@ void vk_get_swapchain_images(struct vk_state *state) {
 }
 
 /**
- * @brief Creates a shader module
- * 
- * @param device Vulkan device
- * @param code Shader bytecode
- * @param size Shader bytecode size in bytes
- * @param module Returns the created shader module
- * @return VkResult Vulkan errors
- */
-void vk_create_shader_module(VkDevice device, void *code, uint64_t size,
-  VkShaderModule *module) {
-
-  VkShaderModuleCreateInfo create_info = {0};
-  create_info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-  create_info.codeSize = size;
-  create_info.pCode = code;
-
-  vkCreateShaderModule(device, &create_info, 0, module);
-}
-
-/**
- * @brief Create a Vulkan pipeline layout
- * 
- * @param device Vulkan device
- * @param pipeline Returns the created pipeline layout
- * @return VkResult Vulkan errors
- */
-void vk_create_pipeline_layout(VkDevice device,
-  VkPipelineLayout *pipeline) {
-
-  VkPipelineLayoutCreateInfo create_info = {0};
-  create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-
-  vkCreatePipelineLayout(device, &create_info, 0, pipeline);
-}
-
-/**
  * @brief Create a Vulkan render pass
  * 
  * @param state Vulkan state
@@ -476,120 +443,6 @@ void vk_create_render_pass(struct vk_state *state) {
   create_info.pSubpasses = &subpass;
 
   vkCreateRenderPass(state->device, &create_info, 0, &state->render_pass);
-}
-
-/**
- * @brief Creates a Vulkan graphics pipeline
- * 
- * @param device Vulkan device
- * @param extent Dimensions of framebuffer
- * @param vertex_stage Vertex shader
- * @param fragment_stage Fragment shader
- * @param pipeline Returns the created graphics pipeline
- * @return VkResult Vulkan errors
- */
-void vk_create_graphics_pipeline(VkDevice device,
-  VkPipelineShaderStageCreateInfo vertex_stage,
-  VkPipelineShaderStageCreateInfo fragment_stage,
-  VkRenderPass render_pass, VkPipeline *pipeline) {
-
-  VkPipelineLayout pipeline_layout;
-  vk_create_pipeline_layout(device, &pipeline_layout);
-
-  VkPipelineShaderStageCreateInfo stages[] =
-    {vertex_stage, fragment_stage};
-
-  VkVertexInputBindingDescription bind_desc = {0};
-  bind_desc.binding = 0;
-  bind_desc.stride = sizeof(struct vk_vertex);
-  bind_desc.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-
-  VkVertexInputAttributeDescription attributes[2] = {0};
-  attributes[0].location = 0;
-  attributes[0].binding = 0;
-  attributes[0].format = VK_FORMAT_R32G32_SFLOAT;
-  attributes[0].offset = 0;
-  attributes[1].location = 1;
-  attributes[1].binding = 0;
-  attributes[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-  attributes[1].offset = sizeof(float) * 2;
-
-  VkPipelineVertexInputStateCreateInfo vertex_state = {0};
-  vertex_state.sType =
-    VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-  
-  vertex_state.vertexBindingDescriptionCount = 1;
-  vertex_state.pVertexBindingDescriptions = &bind_desc;
-  vertex_state.vertexAttributeDescriptionCount = 2;
-  vertex_state.pVertexAttributeDescriptions = attributes;
-
-  VkPipelineInputAssemblyStateCreateInfo input_assemble = {0};
-  input_assemble.sType =
-    VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
-
-  input_assemble.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
-  input_assemble.primitiveRestartEnable = 0;
-
-  VkPipelineViewportStateCreateInfo viewport_state = {0};
-  viewport_state.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
-  viewport_state.viewportCount = 1;
-  viewport_state.scissorCount = 1;
-
-  VkPipelineRasterizationStateCreateInfo rasterisation = {0};
-  rasterisation.sType =
-    VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
-
-  rasterisation.depthClampEnable = 0;
-  rasterisation.rasterizerDiscardEnable = 0;
-  rasterisation.polygonMode = VK_POLYGON_MODE_FILL;
-  rasterisation.lineWidth = 1;
-  rasterisation.cullMode = VK_CULL_MODE_BACK_BIT;
-  rasterisation.frontFace = VK_FRONT_FACE_CLOCKWISE;
-  rasterisation.depthBiasEnable = 0;
-
-  VkPipelineMultisampleStateCreateInfo multisample = {0};
-  multisample.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
-  multisample.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
-  multisample.sampleShadingEnable = 0;
-
-  VkPipelineColorBlendAttachmentState blend_attachments[1] = {0};
-  blend_attachments[0].blendEnable = 0;
-  blend_attachments[0].colorWriteMask = VK_COLOR_COMPONENT_R_BIT
-    | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT
-    | VK_COLOR_COMPONENT_A_BIT;
-
-  VkPipelineColorBlendStateCreateInfo colour_blend = {0};
-  colour_blend.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
-  colour_blend.logicOpEnable = 0;
-  colour_blend.attachmentCount = SIZEOF_ARRAY(blend_attachments);
-  colour_blend.pAttachments = blend_attachments;
-
-  VkDynamicState dynamic_states[] = {
-    VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR
-  };
-
-  VkPipelineDynamicStateCreateInfo dynamic_state = {0};
-  dynamic_state.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
-  dynamic_state.dynamicStateCount = SIZEOF_ARRAY(dynamic_states);
-  dynamic_state.pDynamicStates = dynamic_states;
-
-  VkGraphicsPipelineCreateInfo create_info = {0};
-  create_info.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
-  create_info.stageCount = 2;
-  create_info.pStages = stages;
-  create_info.pVertexInputState = &vertex_state;
-  create_info.pInputAssemblyState = &input_assemble;
-  create_info.pViewportState = &viewport_state;
-  create_info.pRasterizationState = &rasterisation;
-  create_info.pMultisampleState = &multisample;
-  create_info.pColorBlendState = &colour_blend;
-  create_info.pDynamicState = &dynamic_state;
-  create_info.layout = pipeline_layout;
-  create_info.renderPass = render_pass;
-  create_info.subpass = 0;
-
-  vkCreateGraphicsPipelines(device, VK_NULL_HANDLE,
-    1, &create_info, 0, pipeline);
 }
 
 /**
@@ -700,9 +553,5 @@ struct vk_state vk_init(struct vk_surface surface) {
 
   return state;
 }
-
-#include "vulkan_render.c"
-#include "vulkan_shader.c"
-#include "vulkan_buffer.c"
 
 #endif  // SLN_VULKAN
